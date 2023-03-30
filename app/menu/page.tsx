0eb56fb64b2f4dd0f7fa9cccc96d177b22384248
Darrell-Rafa <drafa0609@gmail.com>
2023-03-30 19:02:41 +0700

    add navigation to menu

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -2,6 +2,7 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Hero from "../components/menu/Hero";
 import Menus from "../components/menu/Menus";
+import Navigation from "../components/menu/Navigation";
 import Sidebar from "../components/menu/Sidebar";
 
 function page() {
@@ -9,7 +10,8 @@ function page() {
     <main className="min-h-screen">
       <Header />
       <Hero />
-      <div className="flex gap-8 mt-16">
+      <Navigation />
+      <div className="flex gap-8">
         <Sidebar />
         <Menus />
       </div>
